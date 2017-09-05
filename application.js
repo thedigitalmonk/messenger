56bebb181f7d2624384bffa42d8c6ff95c1d6e69
thedigitalmonk <hello@thedigitalmonk.in>
2017-09-06 01:14:56 +0530

    changed back to localhost for dev testing

--- application.js
+++ application.js
@@ -11,7 +11,8 @@ var messageRoutes = require('./routes/messages');
 var userRoutes = require('./routes/user');
 
 var app = express();
-mongoose.connect('test-user:abc123@ds125774.mlab.com:25774/messages-app');
+//mongoose.connect('test-user:abc123@ds125774.mlab.com:25774/messages-app');
+mongoose.connect('localhost:27017/node-angular');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
--- routes/messages.js
+++ routes/messages.js
@@ -1,10 +1,9 @@
 var express = require('express');
 var router = express.Router();
+var jwt = require('jsonwebtoken');
 
 var Message = require('../models/message');
 var User = require('../models/user');
-
-var jwt = require('jsonwebtoken');
 
 router.get('/', function (req, res, next) {
     Message.find()
